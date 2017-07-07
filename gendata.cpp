dcbd1c9bbc9f2a041f13b74be4190533fd8bfb51
nishad <nmaskara@caltech.edu>
2017-07-06 17:32:12 -0700

    update files

--- gendata.cpp
+++ gendata.cpp
@@ -11,24 +11,29 @@
 #include <time.h>
 #include <random>
 
-void writeTestData(string fname, int numtrials, double p) {
-	int nrows = 4;
-	int ncols = 4;
+void writeTestData(string fname, string type, int latsize, int numtrials, double p) {
+	int nrows = latsize;
+	int ncols = latsize;
 	ofstream out(fname);
-	Lattice L(nrows, ncols);
+	Base_Lattice * L;
 	Decoder D;
 	mt19937 mtrand;
-
+	if (type == "square")
+		L = new Lattice(nrows, ncols);
+	if (type == "triangle")
+		L = new Triangle(nrows, ncols);
+	if (type == "Hexagonal")
+		L = new Hexagonal(nrows, ncols);
 	for (int i = 0; i < numtrials; i++) {
 		if (p < 0)	p = (double) mtrand() / mtrand.max();
-		L.clear();
-		L.genCorrPairErrs(p, 0);
-		L.checkErrors();
-		vector<int> errors = L.getCheck();
-		pairlist matching = D.matchTopLeft(L.getErrors());
+		L->clear();
+		L->genCorrPairErrs(p, 0);
+		L->checkErrors();
+		vector<int> errors = L->getCheck();
+		pairlist matching = D.matchTopLeft(L->getErrors());
 		//L.printLattice();
-		L.applyCorrection(matching);
-		int r2 = L.checkCorrection();
+		L->applyCorrection(matching);
+		int r2 = L->checkCorrection();
 		/*if (L.getErrors().size() == 0) {
 			cout << r2 << endl;
 			if (r2 != 0){
@@ -36,14 +41,14 @@ void writeTestData(string fname, int numtrials, double p) {
 				abort();
 			}
 		}*/
-		L.checkErrors();
-		assert(L.getErrors().size() == 0);
-		int result = L.checkCorrection();
+		L->checkErrors();
+		assert(L->getErrors().size() == 0);
+		int result = L->checkCorrection();
 		assert(result == r2);
 		//L.printLattice();
 		///cout << result << endl;
 		//cout << L.nerrs << endl;
-		for (int i = 0; i < L.nerrs; i++) {
+		for (int i = 0; i < L->nerrs; i++) {
 			out << errors[i] << ", ";
 		}
 		for (int i = 0; i < 4; i++) {
@@ -55,13 +60,20 @@ void writeTestData(string fname, int numtrials, double p) {
 		out << endl;
 	}
 	out.close();
+	delete L;
 }
 
 
 int main(int argc, char** argv) {
-	if (argc != 4) {
-		cout << "usage: outfile numtrials error_rate" << endl;
+	if (argc != 5) {
+		cout << "usage: type latsize numtrials error_rate" << endl;
 		abort();
 	}
-	writeTestData(argv[1], atoi(argv[2]), atof(argv[3]));
+	string type = argv[1];
+	int latsize = atoi(argv[2]);
+	int numtrials = atoi(argv[3]);
+	float error_rate = atof(argv[4]);
+	string filename = "data/" + type + "_" + to_string(latsize) + "_" + to_string(numtrials) + 
+		"_" + to_string(int(error_rate * 1000)) + ".csv";
+	writeTestData(filename, type, latsize, numtrials, error_rate);
 }
--- trainmodel.py
+++ trainmodel.py
@@ -1,29 +1,50 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.regularizers import l2
+from keras import optimizers
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import os
+import errno
+
+def make_exist(path):
+	try:
+		os.makedirs(path)
+	except OSError as exception:
+		if exception.errno != errno.EEXIST:
+			raise
 
 
-filename = 'square_4_4_p_20'
-sqdata = np.genfromtxt("data/" + filename + ".csv", delimiter=',')
-numerrs = np.sum(sqdata[:,0:9], axis=1)
-x,y = np.where(sqdata[:,9:13])
 
+if __name__ == "__main__":
+	if (len(sys.argv) != 7):
+		print "usage: type latsize numtrials error_rate numnodes batchsize"
+		sys.exit()
+	lattype = sys.argv[1]
+	latsize = int(sys.argv[2])
+	numtrials = int(sys.argv[3])
+	p = float(sys.argv[4])
+	numnodes = int(sys.argv[5])
+	batchsize = int(sys.argv[6])
 
+	filename = lattype + "_" + str(latsize) + "_" + str(numtrials) + "_" + str(int(p*1000))
+ 	sqdata = np.genfromtxt("data/" + filename + ".csv", delimiter=',')
+	
+	insize = latsize * latsize
+	model = Sequential()
+	layer1 = Dense(units=numnodes, activation='relu',input_dim=insize)
+	layer2 = Dense(units=4, activation='softmax')
+	model.add(layer1)
+	model.add(layer2)
+	#sgd = optimizers.SGD(lr=0.1, decay=1e-3)
+	model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
-
-
-
-insize = 16
-model = Sequential()
-layer1 = Dense(units=100, activation='relu',input_dim=insize)
-layer2 = Dense(units=4, activation='softmax')
-model.add(layer1)
-model.add(layer2)
-model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-
-early_stopping = EarlyStopping(monitor='val_loss', patience=5)
-
-model.fit(sqdata[:,0:insize], sqdata[:,insize:insize+4], batch_size=32, epochs=400, validation_split=0.3, callbacks=[early_stopping])
-model.save("models/" + filename + ".h5")
+	early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+	make_exist("models/" + filename + "_" + str(numnodes) + "_" + str(batchsize))
+	filepath = "models/" + filename + "_" + str(numnodes) + "_" + str(batchsize) + \
+			"/{epoch:02d}_{loss:.04f}_{acc:.04f}_{val_loss:.04f}_{val_acc:.4f}.h5"
+	checkpt = ModelCheckpoint(filepath)
+	model.fit(sqdata[:,0:insize], sqdata[:,insize:insize+4], batch_size=batchsize,  \
+		epochs=400, validation_split=0.3, callbacks=[early_stopping, checkpt])
