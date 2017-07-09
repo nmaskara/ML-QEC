6733e5e8b0c3d54cfc7782afe71e361ffd260e2d
nishad <nmaskara@caltech.edu>
2017-07-08 20:59:55 -0700

    chaned

--- optimize5.sh
+++ optimize5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
-latsize=5
-numtrials=1000000
+latsize=4
+numtrials=200000
 p=0.05
 ptxt=50
 filename="results.csv"
@@ -11,10 +11,11 @@ for learningrate in 0.01
 do
 	for batchsize in 1024
 	do
-		for numnodes in 2000
+		for numnodes in 1000
 		do
 			echo training $learningrate $batchsize $numnodes
 			python trainmodel.py square $latsize $numtrials $p $numnodes $batchsize $learningrate $filename
 		done
 	done
 done
+sudo shutdown -h now
