b50b5c7f86fde265defe4f299862d81b7f10a7aa
Ubuntu <ubuntu@ip-172-31-27-34.ec2.internal>
2017-07-10 06:29:38 +0000

    none

--- optimize5.sh
+++ optimize5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
-latsize=5
-numtrials=1000000
+latsize=4
+numtrials=200000
 p=0.05
 ptxt=50
 filename="results.csv"
@@ -11,10 +11,12 @@ for learningrate in 0.01
 do
 	for batchsize in 1024
 	do
-		for numnodes in 2000
+		for numnodes in 800
 		do
 			echo training $learningrate $batchsize $numnodes
 			python trainmodel.py square $latsize $numtrials $p $numnodes $batchsize $learningrate $filename
 		done
 	done
 done
+aws s3 cp results.csv s3://nmaskara-models/results/4_800.csv
+sudo shutdown -h now
