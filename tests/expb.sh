f840b8ace8d02d1a3c63e5997bac9e9f6c9fc014
nishad <nmaskara@caltech.edu>
2017-07-11 15:36:43 -0700

    changed

--- tests/expb.sh
+++ tests/expb.sh
@@ -6,9 +6,9 @@ ptxt=100
 
 ./gendata square $latsize $numtrials $p1 -r
 ./gendata square $latsize 100000 $p1 -r
-python trainmodel.py square $latsize $numtrials 300 1000 0.005 \
+python trainmodel.py square $latsize $numtrials 300 1000 0.01 \
 	square_5_10000000_200_random square_5_100000_200_random
-aws s3 cp models/square_5_10000000_200_random_300_1000_5.hdf5 \
-	s3://nmaskara-models/square_5_10000000_200_random_300_1000_5.hdf5
-aws s3 cp log2.txt s3://nmaskara-models/results/log2.txt
+aws s3 cp models/square_5_10000000_200_random_300_1000_10.hdf5 \
+	s3://nmaskara-models/square_5_10000000_200_random_300_1000_10b.hdf5
+aws s3 cp log1.txt s3://nmaskara-models/results/log1b.txt
 sudo shutdown -h now
