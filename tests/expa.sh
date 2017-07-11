3aa839ea007058b8d9b0ee0cbdc21b4950a4b278
nishad <nmaskara@caltech.edu>
2017-07-11 00:42:39 -0700

    fixed errors

--- tests/expa.sh
+++ tests/expa.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -x
 latsize=5
-numtrials=1000000
+numtrials=10000000
 p1=0.2
 ptxt=100
 
 ./gendata square $latsize $numtrials $p1 -r
 ./gendata square $latsize 100000 $p1 -r
 python trainmodel.py square $latsize $numtrials 300 1000 0.01 \
-	square_5_1000000_200_random square_5_1000000_200_random
-aws s3 cp models/square_5_1000000_200_300_1000_10.hdf5 \
-	s3://nmaskara-models/square_5_1000000_200_300_1000_10.hdf5
-shutdown -h now
+	square_5_10000000_200_random square_5_100000_200_random
+aws s3 cp models/square_5_10000000_200_300_1000_10.hdf5 \
+	s3://nmaskara-models/square_5_10000000_200_300_1000_10.hdf5
+aws s3 cp log1.txt s3://nmaskara-models/results/log1.txt
+sudo shutdown -h now
