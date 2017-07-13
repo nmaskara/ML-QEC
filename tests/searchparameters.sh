34d086c98ed4df9ef4dfdbb9be9d40c5a9675e51
nishad <nmaskara@caltech.edu>
2017-07-13 13:07:36 -0700

    streamlined

---
+++ tests/searchparameters.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+lattype='square'
+latsize=3
+p=0.1
+ptxt=100
+
+#batchsize=100
+learningrate=0.01
+lrtxt=10
+numlayers=1
+datasize=$1
+valsize=$((datasize/10 + 1))
+
+./gendata $lattype $latsize $valsize $p
+valname=$lattype'_'$latsize'_'$valsize'_'$ptxt
+
+./gendata $lattype $latsize $datasize $p
+dataname=$lattype'_'$latsize'_'$datasize'_'$ptxt
+batchsize=$((datasize/1000))
+for numnodes in 5 10 20 50 100 200 500
+do
+	python trainmodel.py $lattype 'sgd' $latsize $datasize $numnodes \
+	$numlayers $batchsize $learningrate $dataname $valname
+
+	aws s3 cp 'models/'$dataname'_'$numnodes'_'$batchsize'_'$lrtxt'_sgd.hdf5' \
+	s3://nmaskara-models/$dataname'_'$numnodes'_'$batchsize'_'$lrtxt'_sgd.hdf5'
+
+	aws s3 cp 'results/'$dataname'_'$numnodes'_'$batchsize'_'$lrtxt'_sgd.csv' \
+	's3://nmaskara-models/results/'$dataname'_'$numnodes'_'$batchsize'_'$lrtxt'_sgd.csv'
+done
+
--- trainmodel.py
+++ trainmodel.py
@@ -29,50 +29,74 @@ def genbatches(filename, insize, chsize, batchsize):
 					vals[ct:ct+batchsize, insize:insize+4])
 				ct += batchsize
 
+def makeModel(input_size, num_nodes, hidden_layers):
+	model = Sequential()
+	layer1 = Dense(units=num_nodes, kernel_initializer='he_normal', input_dim=input_size)
+	model.add(layer1)
+	model.add(Activation('relu'))
+	hidden_layers -= 1
+	while (hidden_layers > 0):
+		model.add(Dense(units=num_nodes, kernel_initializer='he_normal'))
+		model.add(Activation('relu'))
+	layer3 = Dense(units=4, activation='softmax')
+	model.add(layer3)
+	opt = optimizers.SGD()
+	model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+	return model
 
 
 if __name__ == "__main__":
-	if (len(sys.argv) != 10):
-		print "usage: type learning latsize datasize numnodes batchsize learningrate dataname valname"
+	if (len(sys.argv) != 11):
+		print "usage: type opttype latsize datasize numnodes hiddenlayers batchsize learningrate dataname valname"
 		sys.exit()
+
+	# read inputs
 	lattype = sys.argv[1]
 	opttype = sys.argv[2]
 	latsize = int(sys.argv[3])
 	datasize = int(sys.argv[4])
 	numnodes = int(sys.argv[5])
-	batchsize = int(sys.argv[6])
-	learningrate = float(sys.argv[7])
-	#outname = sys.argv[8]
-	filename = sys.argv[8]
-	valname = sys.argv[9]
+	hiddenlayers = int(sys.argv[6])
+	batchsize = int(sys.argv[7])
+	learningrate = float(sys.argv[8])
+	filename = sys.argv[9]
+	valname = sys.argv[10]
 	inname = "data/" + filename + ".csv"
-	#filename = lattype + "_" + str(latsize) + "_" + str(numtrials) + "_" + str(int(p*1000))
- 	sqdata = np.genfromtxt("data/" + valname + ".csv", delimiter=',')
+
+	if not os.path.isfile("data/" + valname + ".csv"):
+		print "Couldn't find validation data"
+		os.system('/gendata square 3 100000 0.1')
+		valname = "data/square_3_100000_100.csv"
+ 	valdata = np.genfromtxt("data/" + valname + ".csv", delimiter=',')
 	
 	insize = latsize * latsize
-	model = Sequential()
-	layer1 = Dense(units=numnodes,kernel_initializer='he_normal', input_dim=insize)
-	layer2 = Dense(units=numnodes,kernel_initializer='he_normal')
-	layer3 = Dense(units=4, activation='softmax')
-	model.add(layer1)
-	#model.add(BatchNormalization())
-	#model.add(Dropout(0.5))
-	model.add(Activation('relu'))
-	model.add(layer2)
-	#model.add(BatchNormalization())
-	#model.add(Dropout(0.5))
-	model.add(Activation('relu'))
-	model.add(layer3)
-	opt = optimizers.SGD(lr=learningrate, nesterov=False)
-	if (learning == "Adam"):
-		opt = optimizers.Adam()
-	model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
-	early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+	# generate model
+	model = makeModel(insize, numnodes, hiddenlayers)
+
+	early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 	#make_exist("models/" + filename + "_" + str(numnodes) + "_" + str(batchsize))
 	filepath = "models/" + filename + "_" + str(numnodes) + \
-		"_" + str(batchsize) + "_" + str(int(1000*learningrate)) + "_" + opttype".hdf5"
+		"_" + str(batchsize) + "_" + str(int(1000*learningrate)) + "_" + opttype + ".hdf5"
 	checkpt = ModelCheckpoint(filepath, save_best_only=True)
-	hist = model.fit_generator(genbatches(inname, insize, 100000, batchsize), datasize/batchsize,  \
-		epochs=1000, callbacks=[early_stopping, checkpt], verbose=1, \
-		validation_data=(sqdata[:,0:insize], sqdata[:,insize:insize+4]))
+
+	hist = model.fit_generator(genbatches(inname, insize, 100000, batchsize), datasize/(batchsize),  \
+		epochs=10000, callbacks=[early_stopping, checkpt], verbose=1, \
+		validation_data=(valdata[:,0:insize], valdata[:,insize:insize+4]))
+
+	outpath = "results/" + filename + "_" + str(numnodes) + "_" + str(hiddenlayers) + \
+		"_" + str(batchsize) + "_" + str(int(1000*learningrate)) + "_" + opttype + ".csv"
+	fout = open(outpath, 'w')
+
+	count = 0
+	samplerate = 1
+	for (loss, acc, val_loss, val_acc) in zip(hist.history['loss'], hist.history['acc'], \
+	hist.history['val_loss'], hist.history['val_acc']):
+		count += 1
+		fout.write(str(count * samplerate) + ', ')
+		fout.write(str(loss) + ', ')
+		fout.write(str(acc) + ', ')
+		fout.write(str(val_loss) + ', ')
+		fout.write(str(val_acc) + ', ')
+		fout.write('\n')
+	fout.close()
