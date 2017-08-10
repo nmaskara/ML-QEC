db2aae68fa2be779e4fb03ea42ca6f0fab2f75b9
nishad <nmaskara@caltech.edu>
2017-08-09 17:16:31 -0700

    updated test9

--- tests/test9.sh
+++ tests/test9.sh
@@ -7,39 +7,39 @@ ptxt=100
 opt='sgd'
 
 batchsize=1000
-act='relu'
-numlayers=3
+numlayers=7
+numnodes=60
 datasize=50000000
 valsize=100000
-steps=1000
-epochs=500
+steps=2500
+epochs=100
 
-dt='sq5act_'$(date '+%Y-%m-%d_%H-%M-%S')
-mkdir 'models/'$dt
-mkdir 'results/'$dt
-
-
+dt='sq5resumetest' #_'$(date '+%Y-%m-%d_%H-%M-%S')
+if [ ! -d 'models/'$dt ]; then
+	mkdir 'models/'$dt
+fi
+if [ ! -d 'results/'$dt ]; then
+	mkdir 'results/'$dt
+fi
 #./gendata $lattype $latsize $valsize $p
 valname=$lattype'_'$latsize'_'$valsize'_'$ptxt
-
 #./gendata $lattype $latsize $datasize $p
 dataname=$lattype'_'$latsize'_'$datasize'_'$ptxt
 #batchsize=$((datasize/1000))
-for numnodes in 400
+for numnodes in 400 500 600
 do
-for numlayers in 5 6 4 3 2
-do
-	python trainmodel.py $lattype $opt $act $latsize $steps $epochs $numnodes \
+	python trainmodel.py $lattype $opt $latsize $steps $epochs $numnodes \
 	$numlayers $batchsize $dataname $valname $dt
 
-	aws s3 cp 'models/'$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$act'_'$opt'.hdf5' \
-	s3://nmaskara-models/$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$act'_'$opt'.hdf5'
+	aws s3 cp 'models/'$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'.hdf5' \
+	s3://nmaskara-models/$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'.hdf5'
 
-	aws s3 cp 'results/'$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$act'_'$opt'.csv' \
-	s3://nmaskara-models/results/$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$act'_'$opt'.csv'
-done
-done
+	aws s3 cp 'models/'$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'_best.hdf5' \
+	s3://nmaskara-models/$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'_best.hdf5'
 
+	aws s3 cp 'results/'$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'.csv' \
+	s3://nmaskara-models/results/$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'.csv'
+done
 
 echo 'done'
 #sudo shutdown -h now
