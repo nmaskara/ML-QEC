8063e9ca908e237111563e3f09b55421e31b2766
nishad <nmaskara@caltech.edu>
2017-09-07 13:59:23 -0700

    fix

--- trainmodel.py
+++ trainmodel.py
@@ -254,6 +254,10 @@ if __name__ == "__main__":
 	filename = lattype + '_' + str(latsize) + '_' + str(datasize) + '_' + str(int(p*1000)) 
 	valname = lattype + '_' + str(latsize) + '_' + str(valsize) + '_' + str(int(p*1000))
 
+	if (pratio > 0):
+		filename += '_' + str(pratio)
+		valname += '_' + str(pratio)
+
 	if (copies > 0):
 		count = 1
 		while count <= copies:
