f4ef7e72bd59ef38db20b67e916c880cb3820b38
nishad <nmaskara@caltech.edu>
2017-09-07 17:25:26 -0700

    update

--- gendata.py
+++ gendata.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os, sys
 
-if len(sys.argv) != 5:
+if len(sys.argv) < 6:
 	print "usage: python gendata.py type latsize datasize p"
 	sys.exit(0)
 
@@ -16,8 +16,13 @@ datasize = int(sys.argv[3])
 ptxt = str(int(1000*float(sys.argv[4])))
 print ptxt
 
-inname = 'data/' + sys.argv[1] + '_' + sys.argv[2] + '_' + str(incsize) + '_' + ptxt + '.csv'
-outname = 'data/'+ sys.argv[1] + '_' + sys.argv[2] + '_' + sys.argv[3] + '_' + ptxt + '.h5'
+cstr = ''
+if len(sys.argv) > 5:
+	if sys.argv[5] == '-c':
+		cstr = '_corr_' + sys.argv[6]
+
+inname = 'data/' + sys.argv[1] + '_' + sys.argv[2] + '_' + str(incsize) + '_' + ptxt + cstr + '.csv'
+outname = 'data/'+ sys.argv[1] + '_' + sys.argv[2] + '_' + sys.argv[3] + '_' + ptxt + cstr + '.h5'
 
 if os.path.exists(outname):
 	os.remove(outname)
@@ -27,7 +32,12 @@ chsize = 100000
 if (datasize < incsize):
 	incsize = datasize
 while (added < datasize):
-	os.system('./gendata ' + sys.argv[1] + ' ' + sys.argv[2] + ' ' + str(incsize) + ' ' + sys.argv[4])
+	cmd = './gendata ' + sys.argv[1] + ' ' + sys.argv[2] + ' ' + str(incsize) + ' ' + sys.argv[4]
+	count = 5
+	while count < len(sys.argv):
+		cmd += ' ' + sys.argv[count]
+	os.system(cmd)
+
 	for df in pd.read_csv(inname, chunksize=chsize):
 		vals = df.values[:,:-1].astype(bool)
 		vals = np.packbits(vals, axis=-1)
