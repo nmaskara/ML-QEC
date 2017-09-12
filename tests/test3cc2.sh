f13c61d19e010866a86acf892e1cc0e8c0021cd2
nishad <nmaskara@caltech.edu>
2017-09-11 19:31:57 -0700

    update

---
+++ tests/test3cc2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+
+lattype='cc2'
+latsize=7
+p=0.1
+ptxt=100
+opt='adam'
+
+batchsize=10000
+numlayers=3
+numnodes=200
+datasize=-1
+valsize=100000
+steps=1000
+epochs=50
+
+dt='cc2_7' #_'$(date '+%Y-%m-%d_%H-%M-%S')
+if [ ! -d 'models/'$dt ]; then
+	mkdir 'models/'$dt
+fi
+if [ ! -d 'results/'$dt ]; then
+	mkdir 'results/'$dt
+fi
+./gendata $lattype $latsize $valsize $p
+valname=$lattype'_'$latsize'_'$valsize'_'$ptxt
+
+#./gendata $lattype $latsize $datasize $p
+dataname=$lattype'_'$latsize'_'$datasize'_'$ptxt
+#python csvtohdf5.py $dataname 
+#batchsize=$((datasize/1000))
+for numlayers in 1
+do
+	python trainmodel.py $lattype $opt $latsize $steps $epochs $numnodes \
+	$numlayers $batchsize $datasize $p $valsize $dt
+
+	aws s3 cp 'models/'$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'.hdf5' \
+	s3://nmaskara-models/$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'.hdf5'
+
+	aws s3 cp 'models/'$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'_best.hdf5' \
+	s3://nmaskara-models/$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'_best.hdf5'
+
+	aws s3 cp 'results/'$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'.csv' \
+	s3://nmaskara-models/results/$dt'/'$dataname'_'$numnodes'_'$numlayers'_'$batchsize'_'$opt'.csv'
+done
+
+echo 'done'
+#sudo shutdown -h now
